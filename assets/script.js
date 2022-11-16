7801c689eaf75ce9687a6f0ed3f3f1e3b3add2d6
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-15 19:30:24 -0700

    added featch functions to get info from api

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
-console.log("js is working!");
+//Variables
+let api = "https://valorant-api.com/v1/agents";
 
-console.log($(this));
+
+//functions
+$(document).ready(function () {
+  function getApi() {
+    fetch(api)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+  getApi();
+});
+//event listeners
