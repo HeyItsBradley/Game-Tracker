bedeca08cc721047865e5c1b5aa3aeb7c58f642c
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-15 22:08:06 -0700

    added char titles under picture

--- assets/script.js
+++ assets/script.js
@@ -25,33 +25,46 @@ $(document).ready(function () {
             let makeDiv = document.createElement("div");
 
             let makeImg = document.createElement("img");
+            let makeInnerDiv = document.createElement("div");
+            let makeCaption = document.createElement("h2");
 
             makeDiv.setAttribute("role", "listbox");
-            makeDiv.style = "width:50%;!important;";
+            // makeDiv.style = "width:50%;!important;";
             makeDiv.className = "carousel-item active";
             makeImg.setAttribute("src", charArr[i].fullPortrait);
             makeImg.className = "d-block w-100";
             makeImg.style = "box-shadow:100px black;";
+            makeInnerDiv.className = "carousel-caption d-none d-md-block";
+            makeCaption.textContent = charArr[i].displayName;
 
             document.getElementById("innerC").appendChild(makeDiv);
 
             makeDiv.appendChild(makeImg);
+            makeDiv.appendChild(makeInnerDiv);
+            makeInnerDiv.appendChild(makeCaption);
             i++;
           }
           let makeDiv = document.createElement("div");
 
           let makeImg = document.createElement("img");
+          let makeInnerDiv = document.createElement("div");
+          let makeCaption = document.createElement("h2");
 
           makeDiv.setAttribute("role", "listbox");
           // makeDiv.style = "width:50%; !important;";
           makeDiv.className = "carousel-item";
           makeImg.setAttribute("src", charArr[i].fullPortrait);
-          makeImg.className = "d-block w-50";
+          makeImg.className = "d-block w-100";
+          makeInnerDiv.className = "carousel-caption d-none d-md-block";
+          makeCaption.textContent = charArr[i].displayName;
 
           document.getElementById("innerC").appendChild(makeDiv);
 
           makeDiv.appendChild(makeImg);
+          makeDiv.appendChild(makeInnerDiv);
+          makeInnerDiv.appendChild(makeCaption);
         }
+        let activeChar = $(".active");
       });
   }
 
