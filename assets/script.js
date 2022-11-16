407fa82259b3b656ccfb569301d041d0fed33355
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-15 21:33:08 -0700

    added working images to carousel

--- assets/script.js
+++ assets/script.js
@@ -11,21 +11,46 @@ $(document).ready(function () {
       })
       .then(function (data) {
         console.log(data);
+        let charArr = [];
         for (let i = 0; i < data.data.length; i++) {
-          let makeOuterDiv = document.createElement("div");
-          let makeInnerDiv = document.createElement("div");
+          charArr.push(data.data[i]);
+        }
+
+        charArr.splice(7, 1);
+        console.log(charArr);
+
+        for (let i = 0; i < charArr.length; i++) {
+          console.log("second loop is runninog");
+          if (i === 0) {
+            let makeDiv = document.createElement("div");
+
+            let makeImg = document.createElement("img");
+
+            makeDiv.setAttribute("role", "listbox");
+            makeDiv.style = "width:50%;!important;";
+            makeDiv.className = "carousel-item active";
+            makeImg.setAttribute("src", charArr[i].fullPortrait);
+            makeImg.className = "d-block w-100";
+            makeImg.style = "box-shadow:100px black;";
+
+            document.getElementById("innerC").appendChild(makeDiv);
+
+            makeDiv.appendChild(makeImg);
+            i++;
+          }
+          let makeDiv = document.createElement("div");
+
           let makeImg = document.createElement("img");
 
-          makeOuterDiv.className = "carousel-inner";
-          // makeOuterDiv.setAttribute("role", "listbox");
-          // makeOuterDiv.style("width:100%; height: 500px !important;");
-          makeInnerDiv.className = "carousel-item";
-          makeImg.setAttribute("src", data.data[i].bustPortrait);
-          makeImg.className = "d-block w-100";
+          makeDiv.setAttribute("role", "listbox");
+          // makeDiv.style = "width:50%; !important;";
+          makeDiv.className = "carousel-item";
+          makeImg.setAttribute("src", charArr[i].fullPortrait);
+          makeImg.className = "d-block w-50";
 
-          document.getElementById("bigImg").appendChild(makeOuterDiv);
-          makeOuterDiv.appendChild(makeInnerDiv);
-          makeInnerDiv.appendChild(makeImg);
+          document.getElementById("innerC").appendChild(makeDiv);
+
+          makeDiv.appendChild(makeImg);
         }
       });
   }
