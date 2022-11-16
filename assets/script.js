852e3dc1ceface8665c6a918eac245696435b540
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-15 20:23:49 -0700

    added character pictures to a carousel

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 //Variables
 let api = "https://valorant-api.com/v1/agents";
-
+let apiData = "";
 
 //functions
 $(document).ready(function () {
@@ -11,8 +11,25 @@ $(document).ready(function () {
       })
       .then(function (data) {
         console.log(data);
+        for (let i = 0; i < data.data.length; i++) {
+          let makeOuterDiv = document.createElement("div");
+          let makeInnerDiv = document.createElement("div");
+          let makeImg = document.createElement("img");
+
+          makeOuterDiv.className = "carousel-inner";
+          // makeOuterDiv.setAttribute("role", "listbox");
+          // makeOuterDiv.style("width:100%; height: 500px !important;");
+          makeInnerDiv.className = "carousel-item";
+          makeImg.setAttribute("src", data.data[i].bustPortrait);
+          makeImg.className = "d-block w-100";
+
+          document.getElementById("bigImg").appendChild(makeOuterDiv);
+          makeOuterDiv.appendChild(makeInnerDiv);
+          makeInnerDiv.appendChild(makeImg);
+        }
       });
   }
+
   getApi();
 });
 //event listeners
